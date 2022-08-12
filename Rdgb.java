12f0ac956bc84a53e0539a4023e4ba134c16b20f
Pradeepajb <pradeepapradeepa358@gmail.com>
2022-08-12 14:13:46 +0530

    new code

---
+++ Rdgb.java
@@ -0,0 +1,30 @@
+package Variaties import Education.Arts();
+
+public class Rdgb  {
+private void colorR()  {
+	System.out.println("RED");
+
+}
+private void colord()  {
+	System.out.println("Dark Pink");
+}
+private void colorg() {
+	System.out.println("Green");
+
+}
+private void colorb() {
+	System.out.println("Blue");
+}
+public static void main(String[] args) {
+	Rdgb c= new Rdgb();
+	c.colorb();
+	c.colord();
+	c.colorg();
+	c.colorb();
+	Arts a= new Arts();
+	a.
+}
+CompanyInfo o =new companyInfo();
+o.CompanyNamme();
+}
+}
---
+++ src/Variaties/Rdgb.java
@@ -0,0 +1,25 @@
+package Variaties;
+
+public class Rdgb {
+private void colorR()  {
+	System.out.println("RED");
+
+}
+private void colord()  {
+	System.out.println("Dark Pink");
+}
+private void colorg() {
+	System.out.println("Green");
+
+}
+private void colorb() {
+	System.out.println("Blue");
+}
+public static void main(String[] args) {
+	Rdgb c= new Rdgb();
+	c.colorb();
+	c.colord();
+	c.colorg();
+	c.colorb();
+}
+}
